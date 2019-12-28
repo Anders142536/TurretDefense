d099ab7d58c4d704af709071fea1037c34ae2ea0
Anders <anders142536@gmail.com>
2019-12-28 19:45:25 +0100

    first push of working project. setup basic level with one enemy walking on start of level

---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Enemy;
+    private Transform EnemiesParent;
+    private List<GameObject> enemies;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("starting game manager");
+        enemies = new List<GameObject>();
+        EnemiesParent = new GameObject("Board").transform;
+        GameObject newEnemyInstance = Instantiate(Enemy, new Vector3(-15f, 0f, 0f), Quaternion.identity) as GameObject;
+
+        enemies.Add(newEnemyInstance);
+        newEnemyInstance.transform.SetParent(EnemiesParent);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject enemy in enemies)
+        {
+            Vector2 position = enemy.transform.position;
+            Vector2 newPosition = new Vector2(position.x + 1 * Time.deltaTime, position.y);
+            enemy.transform.position = newPosition;
+        }
+    }
+}
